8aa05818be524b2f39eca8e167e03beb9882613e
zhangflg <password123>
2018-05-18 18:14:09 +0800

    广播——电池监测

--- app/src/main/java/com/arms/zfl/androidlearning/MainActivity.kt
+++ app/src/main/java/com/arms/zfl/androidlearning/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.arms.zfl.androidlearning
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    lateinit var batteryReceiver: BatteryInfoReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        batteryReceiver = BatteryInfoReceiver()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerBatteryInfo()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterBatteryInfo()
+    }
+
+    private fun registerBatteryInfo() {
+        val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        registerReceiver(batteryReceiver, filter)
+    }
+
+    private fun unregisterBatteryInfo() {
+        unregisterReceiver(batteryReceiver)
+    }
+
+    class BatteryInfoReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                Log.e("BatteryInfoReceiver", "battery changed action" + (intent?.action ?: ""))
+            }
+        }
+
     }
 }
